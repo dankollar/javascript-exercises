e7fc29fcc9db23b3038f2bbdbb4a0944785f3f83
Dan Kollar <danielkollar@outlook.com>
2022-04-27 20:23:36 -0400

    Adds solution to reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split('');
+    stringArray.reverse();
+    return stringArray.join('');
 };
+
+reverseString('hello there');
 
 // Do not edit below this line
 module.exports = reverseString;
