ba48a6540fbeedd039788d66fac65b4a7249d2fc
Dan Kollar <danielkollar@outlook.com>
2022-04-27 20:10:36 -0400

    Adds code for first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, int) {
+    let stringArray = [];
+    if  (int < 0){
+        return "ERROR";
+    }
+    for (i = 1; i <= int; i++){
+        stringArray.push(string);
+    }
+    return stringArray.join('');
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
